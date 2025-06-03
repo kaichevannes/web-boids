f144dec5f3a4eb1d7994b0de7a73bf5b86173c67
Kai Chevannes <chevannes.kai@gmail.com>
2025-06-03 18:46:15 +0100

    feat: add pkg files to git
    normally these would be gitignored but since this will be used as a
    private package that isn't published to npm, i need the files to be in
    source control.

---
+++ pkg/web_boids.js
@@ -0,0 +1,5 @@
+import * as wasm from "./web_boids_bg.wasm";
+export * from "./web_boids_bg.js";
+import { __wbg_set_wasm } from "./web_boids_bg.js";
+__wbg_set_wasm(wasm);
+wasm.__wbindgen_start();
